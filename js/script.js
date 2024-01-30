afe5b0625c254d3460798769baf42ccd9e0fce21
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2024-01-30 10:05:57 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
 /** @format */
+/***********************menu *******************************/
+let openMenu = document.querySelector(".openMenu");
+let closeMenu = document.querySelector(".close");
+let menu = document.querySelector(".sid_nav_barre");
+openMenu.addEventListener("click", () => {
+  menu.classList.add("sid_nav_barrejs");
+});
+closeMenu.addEventListener("click", () => {
+  menu.classList.remove("sid_nav_barrejs");
+});
 
+/*********************************************** */
+let link = document.querySelectorAll(".link");
+for (let x = 0; x < link.length; x++) {
+  link[x].addEventListener("click", () => {
+    menu.classList.remove("sid_nav_barrejs");
+  });
+}
 /******************** GETION DES INTERACTION DES IMAGES DES MEMBRES DE L'EQUIPE********************************* */
 let article = document.querySelectorAll("article");
 for (let j = 0; j < article.length; j++) {
   article[j].addEventListener("mouseover", () => {
-    article[j].classList.add("articleImg");
+    article[j].classList.add("articleJSeffet");
   });
 
   article[j].addEventListener("mouseout", () => {
-    article[j].classList.remove("articleImg");
+    article[j].classList.remove("articleJSeffet");
   });
 }
