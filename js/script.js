6b16aed875c2190d2ab853d94f492535d2089329
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2024-03-05 09:08:47 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -30,6 +30,7 @@ for (let j = 0; j < article.length; j++) {
   });
 }
 /********************************appel de l'API************************** */
+/*
 let maps = document.getElementById("maps");
 fetch(
   "https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1"
@@ -40,3 +41,4 @@ fetch(
     maps.alt = ":(";
   }
 });
+*/
